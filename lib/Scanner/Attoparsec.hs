c55e8de7f96a038f5e46f0c9ba2e0fb1282cc892
Yuras <shumovichy@gmail.com>
2021-04-03 02:09:59 +0300

    Inject scanner into attoparsec (#3)

--- lib/Scanner/Attoparsec.hs
+++ lib/Scanner/Attoparsec.hs
@@ -1,12 +1,15 @@
 
 module Scanner.Attoparsec
 ( atto
+, toAtto
 )
 where
 
 import qualified Scanner
 import Scanner.Internal (Scanner (Scanner))
 
+import Data.Maybe
+import qualified Data.ByteString as ByteString
 import qualified Data.Attoparsec.ByteString as Atto
 
 {-# INLINE atto #-}
@@ -23,3 +26,22 @@ atto p = Scanner $ \bs next ->
       Atto.Done bs' r -> next bs' r
       Atto.Fail bs' _ err -> Scanner.Fail bs' err
       Atto.Partial cont' -> slowPath cont' next
+
+-- | Convert scanner to attoparsec parser
+--
+-- @since 0.2
+toAtto :: Scanner a -> Atto.Parser a
+toAtto s = go (Scanner.scan s)
+  where
+  go run = do
+    chunk <- fromMaybe ByteString.empty <$> Atto.getChunk
+    case run chunk of
+      Scanner.Done bs r -> do
+        _ <- Atto.take (ByteString.length chunk - ByteString.length bs)
+        return r
+      Scanner.Fail bs msg -> do
+        _ <- Atto.take (ByteString.length chunk - ByteString.length bs)
+        fail msg
+      Scanner.More next -> do
+        _ <- Atto.take (ByteString.length chunk)
+        go next
